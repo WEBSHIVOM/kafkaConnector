44944a0ca4e3ae248c8d44c4d899ef83ea6551e0
WEBSHIVOM <shivompandey636@gmail.com>
2023-06-03 11:32:00 +0530

    kakfa spring

---
+++ src/main/java/com/webom/kakfaConnector/KakfaConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.webom.kakfaConnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakfaConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakfaConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webom/kakfaConnector/KakfaConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webom.kakfaConnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakfaConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
